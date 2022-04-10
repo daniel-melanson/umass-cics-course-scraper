05bc01b3aa6e3f3c29a6a71831f968f425ec8714
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-10 18:32:11 -0400

    feat: math course scraping

--- src/scraper/web.py
+++ src/scraper/web.py
@@ -37,9 +37,11 @@ class RawCourse(TypedDict):
     title: str
     most_recent_offering: str
     past_offerings: list[str]
-    description: Optional[str]
+    description: str
     semester_staff: dict[str, str]
     frequency: Optional[str]
+    credits: Optional[str]
+    prerequisites: Optional[str]
 
 
 def scrape_courses() -> dict[str, RawCourse]:
@@ -78,9 +80,12 @@ def _scrape_cics_courses(courses: dict[str, RawCourse]):
 
             for header in soup.select("h2:not(:first-child)"):
                 raw_title = get_tag_text(header.select_one(":first-child"))
+                assert raw_title
+
                 log.debug("Matching course header: %s", raw_title)
                 title_match = re.match(r"^(CICS|COMPSCI|INFO(SEC)?)\s*(\w+):\s*([\w -:]+)", raw_title, re.I)
                 assert title_match
+                log.debug("Matched: %s", title_match)
 
                 course_subject = title_match.group(1).upper()
                 if course_subject == "INFOSEC":
@@ -89,16 +94,19 @@ def _scrape_cics_courses(courses: dict[str, RawCourse]):
 
                 course_number = title_match.group(3).upper()
                 course_id = course_subject + " " + course_number
-                log.debug("Got course id: %s", course_id)
+                log.debug("Scraping course: %s", course_id)
 
                 if course_id in courses:
-                    log.debug("Found past entry...")
+                    log.debug("Found past entry.")
                     course = courses[course_id]
 
                     course["past_offerings"].append(semester)
                 else:
                     log.debug("Initalizing new course dictionary...")
-                    course_description = get_tag_text(header.find_next_sibling("p"))
+                    paragraph = header.find_next_sibling("p")
+                    assert paragraph
+
+                    course_description = get_tag_text(paragraph)
 
                     course = RawCourse(
                         id=course_id,
@@ -138,6 +146,93 @@ def _scrape_cics_courses(courses: dict[str, RawCourse]):
 
 def _scrape_math_courses(courses: dict[str, RawCourse]):
     log.info("Scraping mathematics courses...")
+
+    soup = fetch_soup("https://www.math.umass.edu/course-descriptions")
+    first_option = soup.select_one("#edit-semester-tid > option[selected='selected']")
+
+    start = int(first_option.attrs['value'])
+    log.debug("Starting from semester id: %s", start)
+
+    for i in range(start - 10, start + 1):
+        log.debug("Found semester id: %s", i)
+        soup = fetch_soup(f"https://www.math.umass.edu/course-descriptions?semester_tid={i}")
+
+        articles = soup.select("div.views-row > article")
+        if len(articles) == 0:
+            log.debug("No articles found, skipping.")
+            continue
+
+        selected_option = soup.select_one("#edit-semester-tid > option[selected='selected']")
+        assert selected_option
+
+        semester = get_tag_text(selected_option)
+
+        log.debug("Scraping mathematics courses for %s...", semester)
+        for article in articles:
+            title_tag = article.select_one("div[class^='field-title']")
+            assert title_tag
+
+            raw_title = get_tag_text(title_tag)
+
+            log.debug("Matching title: %s", raw_title)
+            title_match = re.match(
+                r'^(MATH|STAT|HONORS)\s*(\w+)(\.\d*)?:\s*([\w -:]+)',
+                raw_title,
+                re.I
+            )
+            assert title_match
+            log.debug("Matched: %s", title_match)
+
+            if title_match.group(1) == 'HONORS':
+                log.debug("Skipping due to HONORS subject.")
+                continue
+
+            course_subject = title_match.group(1).upper()
+            if course_subject == 'STAT':
+                course_subject = 'STATISTIC'
+
+            course_number = title_match.group(2).upper()
+            course_id = course_subject + ' ' + course_number
+
+            log.debug("Scraping course: %s", course_id)
+            if course_id in courses:
+                log.debug("Found past entry.")
+                course = courses[course_id]
+
+                if semester != course["most_recent_offering"]:
+                    log.debug("Adding %s to past offerings.", semester)
+                    course["past_offerings"].append(semester)
+            else:
+                log.debug("No past entry found. Aggregating information...")
+                course_title = title_match.group(4)
+                description_tag = article.select_one("div[class^='field-course-descr-description']")
+                assert description_tag
+
+                course_description = get_tag_text(description_tag)
+
+                course = RawCourse(
+                    subject=course_subject,
+                    id=course_id,
+                    number=course_number,
+                    title=course_title,
+                    description=course_description,
+                    most_recent_offering=semester,
+                    past_offerings=[],
+                    semester_staff={},
+                )
+
+                course_prereqs = article.select_one(
+                    "div[class^='field-course-descr-prereq']"
+                )
+                if course_prereqs:
+                    course["prerequisites"] = get_tag_text(course_prereqs)
+                    log.debug("Found prerequisites tag: %s", course["prerequisites"])
+
+                log.debug("Initalized course: %s", course)
+                courses[course_id] = course
+
+        log.debug("Scraped mathematics courses for %s.", semester)
+
     log.info("Scraped mathematics courses.")
 
 
